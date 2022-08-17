925746f54d9662d3d2de9789c1875b396a290dd9
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-17 21:02:28 +0300

    DONE WITH MAVEN FOR TODAY

---
+++ Notes.java
@@ -0,0 +1,17 @@
+/*I AM A LITTLE LATE FOR TAKING NOTES BUT I WILL DO MY BEST
+ * 
+ * WHAT IS MAVEN
+ * Maven is a tool that we java developer use to work with other platform and other languages like
+ * let's say you wanna work with mysql we need to download the Mysql Connector Jar file and 
+ * all these jar files called dependencies so Maven help us to get those Jar files..
+ * 
+ * EVERY TIME YOU WORK WITH MAVEN YOU GONNA HAVE TO MAKE SURE THAT YOU ARE CONNECTED WITH INTERNET
+ * Because everytime you use maven for the first time it has to download the jar files from the remote repository
+ * And aslo it has local repository so that the good thing right, coz we dont have to use it all the time
+ * 
+ * 
+ * 
+ * TO RUN MAVEN WEB APP WITH EMBEDDED JETTY
+ * You gonna have to put some plugins and also you need to use configuration tag
+ * for more info check the pom.xml file in the plugins you will see what I mean
+ */
